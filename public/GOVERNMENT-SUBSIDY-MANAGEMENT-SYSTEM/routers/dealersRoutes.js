094b6054e0279fa1249691569a84319114abc065
harsha-manugunta <126898185+harsha-manugunta@users.noreply.github.com>
2024-01-11 20:58:51 +0530

    Delete public directory

--- public/GOVERNMENT-SUBSIDY-MANAGEMENT-SYSTEM/routers/dealersRoutes.js
+++
@@ -1,9 +0,0 @@
-const express= require('express');
-const Router= express.Router();
-const authController= require('./../controllers/authController');
-const dealersController=require('./../controllers/dealersController');
- 
- Router.route('/acceptDeal').patch(dealersController.acceptDeal);
- Router.route('/bargain').patch(dealersController.bargainPrice);
- Router.route('/acceptBargain').patch(dealersController.acceptBargain);
- module.exports=Router;
--- public/js/key.js
+++
@@ -1,2 +0,0 @@
-//Assign the copied API key to the 'key' variable
-key = "1f85fd31c5c11ee2334770e69477bdc9";
